b94a01ab476ca75f345b7a5625075ea5e291c3a3
asghimire <37714894+asghimire@users.noreply.github.com>
2018-03-24 12:07:07 -0400

    Add files via upload

---
+++ 2.1 Assignment-Numeric-Operations.py
@@ -0,0 +1,59 @@
+#Compute the absolute value of y
+y = -95.919741
+print(abs(-95.919741))
+
+#Add x and y and multiply the result by a
+a = 402
+x = 41.151309
+y = -95.919741
+sum = (x+y)*a
+print(sum)
+
+#Calculate the remainder left over after dividing b by a (i.e. b/a)
+a = 402
+b = 1855
+print(b%a)
+
+#Calculate a to the power of 3
+print(a**3)
+
+#Show how to convert a to a floating point number
+float(a)
+print(float(a))
+
+#Multiply x by y and round the result to two significant digits
+print(round((x*y),2))
+
+#Compute the bitwise or of a and b
+print(a|b)
+
+#Compute x divided by negative y
+print(x/(-y))
+
+#Compute a added to b divided by x minus y
+print((a+b)/(x-y))
+
+#Compute the floored quotient of b and x
+print(b//x)
+
+#Pick an integer number between 33 and 126. Print the following information about this number.
+
+#Its binary representation
+z = bin(121)
+print(z)
+print(bin(36))
+
+#Its hexadecimal representation
+print(hex(121))
+
+#Its octal representation
+print (oct(121))
+
+#The character corresponding to its Unicode point codec
+print (chr(121))
+
+a=1
+a+=1
+b=a
+print(b)
+print(int(5.3))
